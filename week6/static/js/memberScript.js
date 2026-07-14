0d531f7244e1b1e9cb4bc929dcefe7ece7bbe74f
juggle-tech <shareitaga@gmail.com>
2026-07-14 11:37:50 +0800

    Fix js loading issue

---
+++ week6/static/js/memberScript.js
@@ -0,0 +1,18 @@
+
+/* Fetch API */
+// Creare Msg
+async function postMessage() {
+    
+}
+
+
+// Get Msg
+async function getMessages() {
+    
+}
+
+
+// Delete Msg
+async function deleteMessage(id) {
+    
+}
