dc8dad8dfaf7effca2eb4a36384d1d40c2fadc4d
juggle-tech <shareitaga@gmail.com>
2026-07-28 15:53:30 +0800

    update testing for async attr

--- week8/async/track.js
+++ week8/async/track.js
@@ -1,11 +1,14 @@
 // track.js
-console.log('track.js downloaded, execution started');
+logTime('track.js downloaded');
+// console.log('track.js downloaded, execution started');
 
 const imageUrl = 'https://picsum.photos/4000/3000';
 
 const xhr = new XMLHttpRequest();
 xhr.open('GET', imageUrl, false);
 xhr.send();
+
+logTime('Get image');
 
 document.getElementById('preview').src = imageUrl;
 
